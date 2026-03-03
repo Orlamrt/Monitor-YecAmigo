0d8fc57e791c0bd461e0864e559aea41ed34d86b
Orlamrt <orlamrtsl@gmail.com>
2026-03-03 00:13:40 -0600

    implementacion login

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -3,11 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth as FacadesAuth;
 
 class loginController extends Controller
 {
-    public function __invoke()
+    public function index()
     {
         return view('login');
+    }
+
+    public function login(Request $request)
+    {
+
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (FacadesAuth::attempt($credentials)){
+            $request->session()->regenerate();
+
+            return redirect()->intended('welcome')
+            ->with('status', 'Entraste al monitor de YecAmigo');
+        }
+
+        return back()->withErrors([
+            'email' => 'Las credenciales no coinciden con nuestros registros.',
+        ])->onlyInput('email');
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\loginController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/login', [loginController::class , 'index'])->name('login');
+Route::post('/login', [loginController::class , 'login'])->name('login.post');
+
 Route::get('/', function () {return view('welcome');});
 
-Route::get('/login', loginController::class)->name('login');
+Route::get('/welcome', function (){return view('welcome');})->middleware('auth');
